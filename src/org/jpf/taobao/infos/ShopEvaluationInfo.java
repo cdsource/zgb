2dbc6d290e4565e05aeb752956334b9523770e65
吴平福 wupf <wupflove@users.noreply.github.com>
2017-11-20 17:50:31 +0800

    Initial commit

---
+++ src/org/jpf/taobao/infos/ShopEvaluationInfo.java
@@ -0,0 +1,77 @@
+/** 
+* @author 吴平福 
+* E-mail:wupf@asiainfo.com 
+* @version 创建时间：2017年3月30日 上午11:58:25 
+* 类说明 
+*/ 
+
+package org.jpf.taobao.infos;
+
+
+
+/**
+ * 
+ */
+public class ShopEvaluationInfo {
+    //"isTmall":true,"delivery":[479,1,375],"description":[486,0,0],"service":[480,1,308]
+    /**
+     * 
+     */
+    public ShopEvaluationInfo() {
+        // TODO Auto-generated constructor stub
+    }
+
+    private String delivery;
+    private String description;
+    private String service;
+    boolean isTmall;
+    /**
+     * @return the delivery
+     */
+    public String getDelivery() {
+        return delivery;
+    }
+    /**
+     * @param delivery the delivery to set
+     */
+    public void setDelivery(String delivery) {
+        this.delivery = delivery;
+    }
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    /**
+     * @return the service
+     */
+    public String getService() {
+        return service;
+    }
+    /**
+     * @param service the service to set
+     */
+    public void setService(String service) {
+        this.service = service;
+    }
+    /**
+     * @return the isTmall
+     */
+    public boolean isTmall() {
+        return isTmall;
+    }
+    /**
+     * @param isTmall the isTmall to set
+     */
+    public void setTmall(boolean isTmall) {
+        this.isTmall = isTmall;
+    }
+
+}
